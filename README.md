d38af779ed14cffa1c91e06d903554b0018cbc63
NTavou <nikolaos.tavoularis@gmail.com>
2016-12-18 21:21:15 +0200

    adding new content

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Python-Practice
+## Python Practice
+
+This is a training Python repo which contains exercises found in books and online. Python 2.7.11 was used to run those exercises. 
--- ordered_dictionary.py
+++ ordered_dictionary.py
@@ -1,3 +1,5 @@
+# How can we sort a dictionary?
+
 from operator import itemgetter
 from collections import OrderedDict
 
@@ -9,11 +11,14 @@ countr_popul['USA'] = 325164000
 countr_popul['Brazil'] = 206839000
 countr_popul['Indonesia'] = 260581000
 
+# Sorting the dictionary from smallest to largest value
 sorted_countr_popul = sorted(countr_popul.items(), key=itemgetter(1))
 
 print sorted_countr_popul
 
+# Printing the dictionary sorted from largest to smallest value
 print sorted_countr_popul[::-1]
+
 
 
 
