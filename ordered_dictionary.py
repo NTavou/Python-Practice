126f4929746e4028a0ebc0c6d11d277b973de133
NTavou <nikolaos.tavoularis@gmail.com>
2016-12-24 21:23:10 +0200

    adding new content

--- ordered_dictionary.py
+++ ordered_dictionary.py
@@ -11,13 +11,23 @@ countr_popul['USA'] = 325164000
 countr_popul['Brazil'] = 206839000
 countr_popul['Indonesia'] = 260581000
 
-# Sorting the dictionary from smallest to largest value
+# Sorting the dictionary from smallest to largest value based on the population
 sorted_countr_popul = sorted(countr_popul.items(), key=itemgetter(1))
 
 print sorted_countr_popul
 
-# Printing the dictionary sorted from largest to smallest value
+# Printing in reverse order
 print sorted_countr_popul[::-1]
+
+# Printing alphabetically(by key)
+alph_countr_popul = sorted(countr_popul.items(), key=itemgetter(0))
+
+print alph_countr_popul
+
+# In reverse order
+
+print alph_countr_popul[::-1]
+
 
 
 
